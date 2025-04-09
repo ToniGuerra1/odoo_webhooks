132e5e5dd6df302a966d6c1d0d267be698d47f82
ToniGuerra1 <tonimanagement5@gmail.com>
2025-04-09 10:21:40 +0200

    Update account_move_inherit.py

--- odoo_webhooks/odoo_webhooks/models/account_move_inherit.py
+++ odoo_webhooks/odoo_webhooks/models/account_move_inherit.py
@@ -15,9 +15,6 @@ class AccountMove(models.Model):
 
     def action_send_webhook_from_invoice(self):
         """ Envía un webhook con los datos de la factura y registra el log. """
-        license_status = self.env['ir.config_parameter'].sudo().get_param('webhook.license_status', 'invalid')
-        if license_status != 'valid':
-            raise UserError("La licencia no es válida. No se puede enviar el Webhook.")
         
         for invoice in self:
             if invoice.webhook_config_id:
